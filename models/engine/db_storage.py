d93f0e1df7e451d27378d117b12b7762c01e8db2
godfreydekew <dekewgodfrey@gmail.com>
2024-03-23 15:52:45 -0700

    fix

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -5,8 +5,12 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 from models.base_model import Base
-
-
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 user = os.environ.get('HBNB_MYSQL_USER')
 password = os.environ.get('HBNB_MYSQL_PWD')
