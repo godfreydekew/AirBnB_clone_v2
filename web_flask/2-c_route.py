4ab10f44cedcb3b62c0e61505890adb6229c8b6b
godfreydekew <dekewgodfrey@gmail.com>
2024-04-19 14:31:51 -0700

    Task 0

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" starts a Flask web application"""
+from flask import Flask
+from markupsafe import escape
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Main route which displays text"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Main route which displays text"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Main route which displays text"""
+    if text:
+        if '_' in text:
+            text.replace('_', ' ')
+
+    return f"C {escape(text)}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
